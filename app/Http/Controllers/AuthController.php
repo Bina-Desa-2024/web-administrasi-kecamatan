2576d037cd14de5b82ae7f693e8076d6151078ff
nana72848e <d1041211005@student.untan.ac.id>
2024-10-07 14:01:47 +0700

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rules\LoginRule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.masuk');
+    }
+
+    public function masuk(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->with('LoginError', 'Login gagal!');
+
+        // $request->validate([
+        //     'email'     => ['required', 'max:64', 'required_with:password', new LoginRule($request->password)],
+        //     'password'  => ['required']
+        // ]);
+
+        // return redirect()->route('dashboard');
+    }
+
+    public function keluar()
+    {
+        Auth::logout();
+        return redirect()->route('home.index');
+    }
+}
